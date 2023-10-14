635b439d0b609c5ae57950d158147871a5e65b53
Karan Durugkar <karan.durugkar@gmail.com>
2023-10-14 18:09:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import UserTable from './UserTable';
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dummy Data</h1>
+      <UserTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UserTable.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+const UserTable = () => {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the JSONPlaceholder API
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => {
+        setUserData(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const columns = [
+    'SNO',
+    'NAME',
+    'USERNAME',
+    'EMAIL',
+    'PHONE',
+    'WEBSITE',
+  ];
+
+  return (
+    <table className="user-table">
+      <thead>
+        <tr>
+          {columns.map((column, index) => (
+            <th key={index}>{column}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {userData.map((user, index) => (
+          <tr key={user.id}>
+            <td>{index + 1}</td>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>{user.email}</td>
+            <td>{user.phone}</td>
+            <td>{user.website}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserTable;
+
